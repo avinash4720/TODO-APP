2367b60d8396338071a4f9e0ae75236d91171a06
avinash4720 <avinash.vijayarangan@gmail.com>
2020-03-25 20:13:08 +0530

    TODO APP

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,7 +1,28 @@
 # todos_controller.rb
 class TodosController < ApplicationController
+  skip_before_action :verify_authenticity_token
     def index
-      render plain: "Hello, this is /todos!"
+      render plain: Todo.order(:id).map{|todo| todo.pls_string}.join("\n")
+    end
+    def show
+      id=params[:id]
+      todo=Todo.find(id)
+      render plain: todo.pls_string
+    end
+    def create
+      todo_text=params[:todo_text]
+      due_date=DateTime.parse(params[:due_date])
+      new_todo=Todo.create!(todo_text: todo_text,due_date: due_date,completed: false)
+      response="Hey you created with id #{new_todo.id}"
+      render plain: response
+    end
+    def update
+      id=params[:id]
+      completed= params[:completed]
+      todo=Todo.find(id)
+      todo.completed= completed
+      todo.save!
+      render plain: "updated todo with completed status to #{completed}"
     end
   end
   
